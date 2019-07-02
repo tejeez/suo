f0c37122a88f7ba6e39e4750592215fd568d8250
Tatu Peltola <tatu.peltola@gmail.com>
2019-07-02 19:10:32 +0300

    Clean up code of the simple receiver

--- libsuo/simple_receiver.c
+++ libsuo/simple_receiver.c
@@ -7,8 +7,7 @@
 
 #define FRAMELEN_MAX 0x900
 #define OVERSAMPLING 4
-//#define USE_LIQUID_SYMSYNC
-#define USE_COMB_SYMSYNC
+#define DEBUG3
 
 static const float pi2f = 6.283185307179586f;
 
@@ -20,31 +19,28 @@ struct simple_receiver {
 	uint64_t syncmask;
 	float nco_1Hz, afc_speed;
 
-	/* State */
+	/* Deframer state */
 	//uint32_t total_samples;
 	uint64_t latest_bits;
 	unsigned framepos, totalbits;
 	bool receiving_frame;
 
-	float freq_min, freq_max, freq_center, freq_adj; /* Used by AFC */
-	float fm_level0, fm_level1; /* Used by AFC */
-	float est_power; /* estimates for metadata */
+	/* AFC state */
+	float freq_min, freq_max, freq_center, freq_adj;
 
+	/* Timing synchronizer state */
 	float ss_comb[OVERSAMPLING];
-
 	float demod_prev;
-	float ss_prev, ss_middle; /* samples for timing synchronizer */
-	bool ss_prevd;
-	uint32_t timing_phase;
 	unsigned ss_p, ss_ps;
+
+	/* General metadata */
+	float est_power;
 
 	/* liquid-dsp objects */
 	nco_crcf l_nco;
 	resamp_crcf l_resamp;
-	//freqdem l_fdem;
 	firfilt_cccf l_fir0, l_fir1;
 	firfilt_rrrf l_eqfir;
-	symsync_rrrf l_symsync;
 
 	/* Callbacks */
 	struct rx_output_code output;
@@ -99,12 +95,12 @@ static void *simple_receiver_init(const void *conf_v)
 	 * to the resampler. This is needed to allocate a big enough array. */
 	self->resampint = ceilf(resamprate);
 
-	/* NCO */
-	/* Limit AFC range to half of symbol rate to keep it
+	/* NCO:
+	 * Limit AFC range to half of symbol rate to keep it
 	 * from wandering too far */
 	self->nco_1Hz = pi2f / c.samplerate;
-	self->freq_min = self->nco_1Hz * (c.centerfreq - 0.1f*c.symbolrate);
-	self->freq_max = self->nco_1Hz * (c.centerfreq + 0.1f*c.symbolrate);
+	self->freq_min = self->nco_1Hz * (c.centerfreq - 0.5f*c.symbolrate);
+	self->freq_max = self->nco_1Hz * (c.centerfreq + 0.5f*c.symbolrate);
 	self->freq_center = self->nco_1Hz * c.centerfreq;
 	self->l_nco = nco_crcf_create(LIQUID_NCO);
 	/* afc_speed is maximum adjustment of frequency per input sample.
@@ -114,22 +110,11 @@ static void *simple_receiver_init(const void *conf_v)
 
 	nco_crcf_set_frequency(self->l_nco, self->freq_center);
 
-	/* Other liquid-dsp objects */
 	/* Matched filters for 0 and 1 */
 	self->l_fir0 = firfilt_cccf_create((float complex*)fixed_mf0, FIXED_MF_LEN);
 	self->l_fir1 = firfilt_cccf_create((float complex*)fixed_mf1, FIXED_MF_LEN);
-	/*self->l_eqfir = firfilt_rrrf_create((float*)
-		(const float[3]){ -.5f, 2.f, -.5f }, 3);*/
 	self->l_eqfir = firfilt_rrrf_create((float*)
 		(const float[5]){ -.5f, 0, 2.f, 0, -.5f }, 5);
-
-	//self->l_fdem = freqdem_create(1.0f);
-
-#ifdef USE_LIQUID_SYMSYNC
-	self->l_symsync = symsync_rrrf_create_rnyquist(LIQUID_FIRFILT_GMSKRX, OVERSAMPLING, 7, 0.5f, 32);
-	//self->l_symsync = symsync_rrrf_create_rnyquist(LIQUID_FIRFILT_RRC, OVERSAMPLING, 7, 1.0f, 32);
-	symsync_rrrf_set_lf_bw(self->l_symsync, 0.0001f);
-#endif
 
 	return self;
 }
@@ -155,8 +140,6 @@ static void simple_deframer_execute(struct simple_receiver *self, unsigned bit)
 		if(framepos == framelen) {
 			self->output.frame(self->output_arg, self->framebuf, framelen, &self->metadata);
 			receiving_frame = 0;
-			//printf("End:   %7.4f %7.4f %7.4f\n", (double)self->fm_dc,(double)self->fm_level0, (double)self->fm_level1);
-			//symsync_rrrf_unlock(self->l_symsync);
 		}
 	} else {
 		receiving_frame = 0;
@@ -175,28 +158,17 @@ static void simple_deframer_execute(struct simple_receiver *self, unsigned bit)
 			/* Syncword found, start saving bits when next bit arrives */
 			framepos = 0;
 			receiving_frame = 1;
-			//printf("Start: %7.4f %7.4f %7.4f\n", (double)self->fm_dc, (double)self->fm_level0, (double)self->fm_level1);
 
 			/* Fill in some metadata at start of the frame */
-
 			self->metadata.cfo = (nco_crcf_get_frequency(self->l_nco)
 				- self->freq_center ) / self->nco_1Hz;
 			self->metadata.rssi = 10.0f * log10f(self->est_power);
-
-#if 0
-			printf("_%d_", syncerrs);
-#endif
-			//symsync_rrrf_lock(self->l_symsync);
+			self->metadata.ber = (float)syncerrs; // not real BER :D
 		}
 	}
 
 	self->receiving_frame = receiving_frame;
 	self->framepos = framepos;
-
-#if 0
-	if((self->totalbits & 63) == 0) putchar('\n');
-	putchar(bit?'x':'.');
-#endif
 
 	self->totalbits++;
 }
@@ -207,16 +179,13 @@ static int simple_receiver_execute(void *arg, const sample_t *samples, size_t ns
 	struct simple_receiver *self = arg;
 
 	/* Copy some often used variables to local variables */
-	float fm_level0 = self->fm_level0, fm_level1 = self->fm_level1, est_power = self->est_power;
-	uint32_t timing_phase = self->timing_phase;
+	float est_power = self->est_power;
 
 	/* Allocate small buffers from stack */
 	sample_t samples2[self->resampint];
 
 	self->metadata.timestamp = timestamp;
 	/* TODO: increment timestamp in loop */
-
-	//printf("%E ", (double)nco_crcf_get_frequency(self->l_nco) - (double)self->freq_center);
 
 	size_t si;
 	for(si = 0; si < nsamp; si++) {
@@ -232,18 +201,18 @@ static int simple_receiver_execute(void *arg, const sample_t *samples, size_t ns
 
 		/* Process output from the resampler one sample at a time */
 		for(si2 = 0; si2 < nsamp2; si2++) {
-			float demod1, demod = 0, synchronized = 0;
+			float synchronized = 0;
 			unsigned nsynchronized = 0;
-
 			sample_t s2 = samples2[si2];
-			sample_t mf0out = 0, mf1out = 0;
-			float power0, power1;
 
 			/*   Demodulation
 			 * ----------------
 			 * Compare the output amplitude of two filters.
 			 * The output seems to have quite strong ISI, so just
 			 * feed it into some ad-hoc FIR "equalizer"... */
+			sample_t mf0out = 0, mf1out = 0;
+			float power0, power1, demodr, demod = 0;
+
 			firfilt_cccf_push(self->l_fir0, s2);
 			firfilt_cccf_push(self->l_fir1, s2);
 
@@ -252,10 +221,11 @@ static int simple_receiver_execute(void *arg, const sample_t *samples, size_t ns
 			power0 = mag2f(mf0out);
 			power1 = mag2f(mf1out);
 
-			demod1 = (power1 - power0) / (power1 + power0);
 			est_power += (power1 + power0 - est_power) * 0.01f;
+			self->est_power = est_power;
 
-			firfilt_rrrf_push(self->l_eqfir, demod1);
+			demodr = (power1 - power0) / (power1 + power0);
+			firfilt_rrrf_push(self->l_eqfir, demodr);
 			firfilt_rrrf_execute(self->l_eqfir, &demod);
 
 
@@ -276,28 +246,6 @@ static int simple_receiver_execute(void *arg, const sample_t *samples, size_t ns
 			}
 
 
-			/* Debugging outputs */
-			int write(int, const void*, size_t);
-			write(3, &demod, sizeof(float));
-			float asdf = nco_crcf_get_frequency(self->l_nco);
-			write(3, &asdf, sizeof(float));
-			asdf = 0;
-			
-
-#ifndef USE_LIQUID_SYMSYNC
-#ifndef USE_COMB_SYMSYNC
-			uint32_t prev_timing_phase = timing_phase;
-			timing_phase += 0x100000000ULL / OVERSAMPLING;
-
-			if(timing_phase < prev_timing_phase) {
-				synchronized = demod;
-				nsynchronized = 1;
-			}
-			if(timing_phase      >= 0x80000000UL &&
-			   prev_timing_phase <  0x80000000UL) {
-				self->ss_middle = demod;
-			}
-#else
 			/* Feed-forward timing synchronizer
 			 * --------------------------------
 			 * Feed a rectified demodulated signal into a comb filter.
@@ -312,7 +260,6 @@ static int simple_receiver_execute(void *arg, const sample_t *samples, size_t ns
 			float comb = self->ss_comb[ss_p];
 
 			comb += (clampf(fabsf(demod), 1.0f) - comb) * 0.03f;
-			write(3, &comb, sizeof(float));
 
 			self->ss_comb[ss_p] = comb;
 			self->ss_p = ss_p;
@@ -330,79 +277,36 @@ static int simple_receiver_execute(void *arg, const sample_t *samples, size_t ns
 					synchronized = demod;
 				}
 			}
-			self->demod_prev = demod;
-#endif
-#else
-			symsync_rrrf_execute(self->l_symsync, &demod, 1, &synchronized, &nsynchronized);
 
-			//synchronized = demod; nsynchronized = 1; // test: bypass synchronizer
-			assert(nsynchronized <= 1);
+
+#ifdef DEBUG3
+			/* Debugging outputs */
+			int write(int, const void*, size_t);
+			write(3, &demod, sizeof(float));
+			float asdf = nco_crcf_get_frequency(self->l_nco);
+			write(3, &asdf, sizeof(float));
+			write(3, &comb, sizeof(float));
+			write(3, &synchronized, sizeof(float));
 #endif
 
+
+			/* Decisions and deframing
+			 * ----------------------- */
 			if(nsynchronized == 1) {
-				asdf = synchronized;
-				//fm_level0 += (synchronized - fm_level0) * .04f;
 				/* Process one output symbol from synchronizer */
 				bool decision;
 
-				float threshold = 0;
-				if(0 && self->receiving_frame)
-					threshold = 0.5f * (fm_level0 + fm_level1);
-
-				if(synchronized >= threshold) {
+				if(synchronized >= 0)
 					decision = 1;
-					fm_level1 += (synchronized - fm_level1) * 0.05f;
-				} else {
+				else
 					decision = 0;
-					fm_level0 += (synchronized - fm_level0) * 0.05f;
-				}
-
-				self->fm_level0 = fm_level0;
-				self->fm_level1 = fm_level1;
-				self->est_power = est_power;
 
 				simple_deframer_execute(self, decision);
-
-#ifndef USE_LIQUID_SYMSYNC
-#ifndef USE_COMB_SYMSYNC
-				/* Timing error estimator
-				 * ----------------------
-				 * If decision is different from the previous
-				 * decision, assume a transition happened.
-				 * Take a sample in between these two symbols,
-				 * which should be roughly equal to the average of
-				 * the surrounding symbols when timing is correct.
-				 * Use the difference from the average as a
-				 * timing error estimate. */
-				if(decision != self->ss_prevd) {
-					// more convenient variable names
-					float s0 = self->ss_prev, s1 = self->ss_middle,
-						  s2 = synchronized, diff, diff_max = 0.5f;
-
-					diff = clampf((s1 - 0.5f*(s0+s2)) / (s2-s0), diff_max);
-
-					//putchar('5'+8*diff);
-					timing_phase += diff * 1000000.0f;
-				}
-
-				self->ss_prev = synchronized;
-				self->ss_prevd = decision;
 			}
-#else
-			}
-#endif
-#else
-			}
-#endif
-			
-			write(3, &asdf, sizeof(float));
+			self->demod_prev = demod;
 		}	
 	}
 
-	//self->fm_level0 = fm_level0;
-	//self->fm_level1 = fm_level1;
-	self->est_power = est_power;
-	self->timing_phase = timing_phase;
 	return 0;
 }
 
